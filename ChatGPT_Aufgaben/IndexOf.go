d9484d9101897eb1220835fe85b03dbce1bb89e5
Noa <molinasarthez@gmail.com>
2025-10-30 16:30:53 +0100

    Add IndexOf function for slice element search
    Implement IndexOf function to find the index of an element in a slice.

---
+++ ChatGPT_Aufgaben/IndexOf.go
@@ -0,0 +1,14 @@
+// indexof.go
+package chatgptaufgaben
+
+// IndexOf liefert den ersten Index von x in nums oder -1, falls x nicht enthalten ist.
+// Randfälle:
+//   - nil oder leere Liste: -1
+func IndexOf(nums []int, x int) int {
+	for i, v := range nums {
+		if v == x {
+			return i
+		}
+	}
+	return -1
+}
