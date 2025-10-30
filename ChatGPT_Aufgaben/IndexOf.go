4cc1d511a0a1710350df34dc8f974e78f1a83185
Noa <molinasarthez@gmail.com>
2025-10-30 16:31:11 +0100

    Create IndexOf function in IndexOf.go
    Implement initial structure for IndexOf function.

---
+++ ChatGPT_Aufgaben/IndexOf.go
@@ -0,0 +1,9 @@
+// indexof.go
+package chatgptaufgaben
+
+// IndexOf liefert den ersten Index von x in nums oder -1, falls x nicht enthalten ist.
+// Randfälle:
+//   - nil oder leere Liste: -1
+func IndexOf(nums []int, x int) int {
+	return 0
+}
