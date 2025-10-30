69c57a01f9cdb248aa8ccb50fa7b60ccc6bd0421
Noa <molinasarthez@gmail.com>
2025-10-30 16:27:59 +0100

    Add MaxList function to find maximum in a slice
    Implement MaxList function to return maximum value from a list of integers.

--- ChatGPT_Aufgaben/MaxList.go
+++ ChatGPT_Aufgaben/MaxList.go
@@ -3,6 +3,14 @@ package chatgptaufgaben
 // Erwartet eine Liste von Zahlen und gibt das Maximum zurück.
 // Wenn die Liste leer ist, wird 0 zurückgegeben.
 func MaxList(nums []int) int {
-
-	return 0
+	if len(nums) == 0 {
+		return 0
+	}
+	max := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if nums[i] > max {
+			max = nums[i]
+		}
+	}
+	return max
 }
