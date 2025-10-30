3b9f38e9ac099a14690b60ae5b69e04e0940c3c3
Noa <molinasarthez@gmail.com>
2025-10-30 15:48:08 +0100

    Erste Aufgabe: MaxList

---
+++ ChatGPT_Aufgaben/MaxList.go
@@ -0,0 +1,8 @@
+package chatgptaufgaben
+
+// Erwartet eine Liste von Zahlen und gibt das Maximum zurück.
+// Wenn die Liste leer ist, wird 0 zurückgegeben.
+func MaxList(nums []int) int {
+
+	return 0
+}
---
+++ ChatGPT_Aufgaben/MaxList_test.go
@@ -0,0 +1,11 @@
+package chatgptaufgaben
+
+import "fmt"
+
+func ExampleMaxList() {
+	l1 := []int{1, 4, 5, 2, 8, 30, 58, 27, 9, 23}
+	fmt.Println(MaxList(l1))
+	//Output:
+	//58
+
+}
